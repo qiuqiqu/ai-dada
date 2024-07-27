30d0dc6917d4dac1756bee6bc379c423515a4360
qiuqiqu <1961237983@qq.com>
2024-07-27 15:12:13 +0800

    智谱AI接入和AI生成题目,AI智能评分

---
+++ src/main/java/com/gy/aida/model/dto/question/AiGenerateQuestionRequest.java
@@ -0,0 +1,26 @@
+package com.gy.aida.model.dto.question;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class AiGenerateQuestionRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long appId;
+
+    /**
+     * 题目数
+     */
+    int questionNumber = 10;
+
+    /**
+     * 选项数
+     */
+    int optionNumber = 2;
+
+    private static final long serialVersionUID = 1L;
+}
